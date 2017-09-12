804e82231380e6e7925b87b2da03c5c96f864891
kumarmb123 <kumarmb123@gmail.com>
2017-09-12 10:41:44 +0530

    Update installation.rb

--- resources/installation.rb
+++ resources/installation.rb
@@ -1,16 +1,4 @@
-# (C) Copyright IBM Corporation 2016.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+
 
 property :source, kind_of: String, required: true
 property :accept_license, kind_of: [TrueClass, FalseClass], default: false
@@ -99,18 +87,7 @@ action :create do
     type 'soft'
     item 'nofile'
     value 10_240
-  end
 
-  set_limit 'mqm' do
-    type 'hard'
-    item 'nofile'
-    value 10_240
-  end
-
-  execute 'Accept the mqlicense' do
-    user 'root'
-    cwd "#{unpack_dir}/MQServer"
-    command './mqlicense.sh -accept -text_only'
   end
 
   # Work around bug in 'yum' cookbook
